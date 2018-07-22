73d41acf1bf85e0c395ccf069fe6e7fe454e55a5
Manuel Odendahl <wesen@ruinwesen.com>
2018-07-22 12:28:19 -0400

    :art: Use only one branch for right/left collision, slightly better slope code.

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -52,7 +52,6 @@ public class Controller2D : MonoBehaviour {
     }
 
     public void Move(Vector3 velocity) {
-        Debug.Log("Move input velocity " + velocity.x / Time.deltaTime);
         _computeCollisions(ref velocity);
 
         Debug.DrawRay(transform.position, velocity * 10.0f, Color.white);
@@ -112,7 +111,7 @@ public class Controller2D : MonoBehaviour {
             float x = _bounds.min.x + i * horizontalInc;
 
             var rayStart = new Vector2(x, direction > 0.0f ? _bounds.max.y : _bounds.min.y);
-            
+
             Ray ray = new Ray(rayStart, Vector2.up * direction);
             if (Physics.Raycast(ray, out hit, absVelocity + SkinWidth, CollisionMask)) {
                 float distance = hit.distance - SkinWidth;
@@ -137,11 +136,18 @@ public class Controller2D : MonoBehaviour {
         }
     }
 
+    private static float ANGLE_COMPARISON_TOLERANCE = 0.01f;
+
     private void _computeHorizontalCollisions(ref Vector3 velocity) {
         Bounds _bounds = _collider.bounds;
         _bounds.Expand(-SkinWidth * 2);
 
         float verticalInc = _bounds.extents.y * 2 / ((float) NumberOfHorizontalRays - 1);
+        float absVelocity = Math.Abs(velocity.x);
+        float direction = Math.Sign(velocity.x);
+
+        bool isSloped = false;
+        float lastSlopeAngle = 0.0f;
 
         // horizontal rays
         // should I do both directions?
@@ -152,59 +158,53 @@ public class Controller2D : MonoBehaviour {
 
             RaycastHit hit;
 
-            if (velocity.x < 0.0f) {
-                var left = new Vector2(_bounds.min.x, y);
-                Ray rayLeft = new Ray(left, Vector2.left);
-                if (Physics.Raycast(rayLeft, out hit, Math.Abs(velocity.x) + SkinWidth, CollisionMask)) {
-                    float distance = hit.distance - SkinWidth;
-                    Distances.Left = Mathf.Min(Distances.Left, distance);
+            var rayStart = direction < 0.0f ? new Vector2(_bounds.min.x, y) : new Vector2(_bounds.max.x, y);
+            Ray ray = new Ray(rayStart, Vector2.right * direction);
+            
+            if (Physics.Raycast(ray, out hit, absVelocity + SkinWidth, CollisionMask)) {
+                float distance = hit.distance - SkinWidth;
+                float angle = Vector3.SignedAngle(hit.normal, Vector3.up, Vector3.forward);
 
-                    if (i == 0) {
-                        SlopeAngles.Left = Vector3.SignedAngle(hit.normal, Vector3.up, Vector3.forward);
+                if (i == 0 || (i > 0 && Math.Abs(angle - lastSlopeAngle) > ANGLE_COMPARISON_TOLERANCE)) {
+                    if (angle < MaxSlopeAngle) {
+                        isSloped = true;
+                        lastSlopeAngle = angle;
 
-                        if (IsSlopedLeft()) {
-                            Vector3 newVelocity = new Vector3(velocity.x, 0.0f, 0.0f);
+                        Vector3 newVelocity = new Vector3(velocity.x, 0.0f, 0.0f);
 
-                            newVelocity = Quaternion.AngleAxis(-SlopeAngles.Left, Vector3.forward) * newVelocity;
-                            newVelocity.y = Mathf.Max(velocity.y, newVelocity.y);
+                        newVelocity = Quaternion.AngleAxis(angle, Vector3.back) * newVelocity;
+                        
+                        // in case we are jumping
+                        newVelocity.y = Mathf.Max(velocity.y, newVelocity.y);
 
-                            velocity = newVelocity;
+                        velocity = newVelocity;
 
-                            // Mark as grounded
-                            Collision |= Directions.Down;
-                        }
+                        // Mark as grounded
+                        Collision |= Directions.Down;
+                    } else {
+                        isSloped = false;
                     }
+                }
 
-                    if (!IsSlopedLeft()) {
+                if (!isSloped) {
+                    if (direction < 0.0f) {
                         Collision |= Directions.Left;
-
-                        // XXX this is where we want to climb the slope
-
-                        velocity.x = Mathf.Max(-distance, velocity.x);
+                    } else {
+                        Collision |= Directions.Right;
                     }
 
-                    Debug.DrawRay(left, Vector2.left * hit.distance, Color.red);
-                } else {
-                    Debug.DrawRay(left, Vector2.left, Color.green);
+                    velocity.x = direction * Mathf.Min(distance, absVelocity);
                 }
-            } else {
-                var right = new Vector2(_bounds.max.x, y);
-                Ray rayRight = new Ray(right, Vector2.right);
-                if (Physics.Raycast(rayRight, out hit, Math.Abs(velocity.x) + SkinWidth, CollisionMask)) {
-                    float distance = hit.distance - SkinWidth;
+
+                if (direction < 0.0f) {
+                    Distances.Left = Mathf.Min(Distances.Left, distance);
+                } else {
                     Distances.Right = Mathf.Min(Distances.Right, distance);
-                    Collision |= Directions.Right;
-
-                    // XXX this is where we want to climb the slope
-                    velocity.x = Mathf.Min(distance, velocity.x);
-                    Debug.DrawRay(right, Vector2.right * hit.distance, Color.red);
-
-                    if (i == 0) {
-                        SlopeAngles.Right = Vector3.SignedAngle(hit.normal, Vector3.up, Vector3.forward);
-                    }
-                } else {
-                    Debug.DrawRay(right, Vector2.right, Color.green);
                 }
+
+                Debug.DrawRay(rayStart, direction * Vector2.right * hit.distance, Color.red);
+            } else {
+                Debug.DrawRay(rayStart, direction * Vector2.right * hit.distance, Color.green);
             }
         }
 
